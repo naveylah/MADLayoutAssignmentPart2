bd606fd5fe566c0ea92b92a800780729f01b0e28
Nabilah Daud <48626338+naveylah@users.noreply.github.com>
2021-06-14 05:09:01 +0800

    Update gridone.dart

--- lib/gridone.dart
+++ lib/gridone.dart
@@ -7,25 +7,170 @@ class gridOne extends StatefulWidget {
 
 // ignore: camel_case_types
 class _gridOneState extends State<gridOne> {
+  String _status = 'off';
+  changeText() {
+    setState(() {
+      _status = 'on';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GridView.count(
-            crossAxisCount: 3,
-            children: List.generate(5, (index) {
-              return new Card(
-                  elevation: 10.0,
-                  margin: EdgeInsets.all(7.0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  child: Container(
-                    child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "$index",
-                          style: TextStyle(fontSize: 24.0),
-                        )),
-                  ));
-            })));
+        body: Center(
+            child: GridView.count(crossAxisCount: 3, children: <Widget>[
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          elevation: 10.0,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.lightbulb_outline_rounded),
+                        iconSize: 70.0,
+                        onPressed: () => changeText()),
+                    IconButton(
+                        icon: Icon(Icons.power_settings_new_outlined),
+                        iconSize: 20.0,
+                        onPressed: () => changeText()),
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    "Light",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "$_status",
+                    style: TextStyle(fontSize: 7),
+                  ),
+
+                  //Text()
+                )
+              ]),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          elevation: 10.0,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.waves_outlined),
+                        iconSize: 70.0,
+                        onPressed: () => changeText()),
+                    IconButton(
+                        icon: Icon(Icons.power_settings_new_outlined),
+                        iconSize: 20.0,
+                        onPressed: () => changeText()),
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    "Fan",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "$_status",
+                    style: TextStyle(fontSize: 7),
+                  ),
+
+                  //Text()
+                )
+              ]),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          elevation: 10.0,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.car_rental),
+                        iconSize: 70.0,
+                        onPressed: () => changeText()),
+                    IconButton(
+                        icon: Icon(Icons.power_settings_new_outlined),
+                        iconSize: 20.0,
+                        onPressed: () => changeText()),
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    "Garage",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "$_status",
+                    style: TextStyle(fontSize: 7),
+                  ),
+
+                  //Text()
+                )
+              ]),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          elevation: 10.0,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.house_outlined),
+                        iconSize: 70.0,
+                        onPressed: () => changeText()),
+                    IconButton(
+                        icon: Icon(Icons.power_settings_new_outlined),
+                        iconSize: 20.0,
+                        onPressed: () => changeText()),
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    "Porch",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "$_status",
+                    style: TextStyle(fontSize: 7),
+                  ),
+
+                  //Text()
+                )
+              ]),
+        ),
+      ),
+    ])));
   }
 }
