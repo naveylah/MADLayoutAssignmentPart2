0f7604a0067939baefcd135be1b749b654e0e03a
Nabilah Daud <48626338+naveylah@users.noreply.github.com>
2021-06-14 05:09:58 +0800

    Update gridthree.dart

--- lib/gridthree.dart
+++ lib/gridthree.dart
@@ -6,8 +6,132 @@ class gridThree extends StatefulWidget {
 }
 
 class _gridThreeState extends State<gridThree> {
+  String _status = 'off';
+  changeText() {
+    setState(() {
+      _status = 'on';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Center(
+            child: GridView.count(crossAxisCount: 3, children: <Widget>[
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          elevation: 10.0,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.lightbulb_outline_rounded),
+                        iconSize: 70.0,
+                        onPressed: () => changeText()),
+                    IconButton(
+                        icon: Icon(Icons.power_settings_new_outlined),
+                        iconSize: 20.0,
+                        onPressed: () => changeText()),
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    "Light",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "$_status",
+                    style: TextStyle(fontSize: 7),
+                  ),
+
+                  //Text()
+                )
+              ]),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          elevation: 10.0,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.bathtub_outlined),
+                        iconSize: 70.0,
+                        onPressed: () => changeText()),
+                    IconButton(
+                        icon: Icon(Icons.power_settings_new_outlined),
+                        iconSize: 20.0,
+                        onPressed: () => changeText()),
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    "Bathroom",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "$_status",
+                    style: TextStyle(fontSize: 7),
+                  ),
+
+                  //Text()
+                )
+              ]),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          elevation: 10.0,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.waves_outlined),
+                        iconSize: 70.0,
+                        onPressed: () => changeText()),
+                    IconButton(
+                        icon: Icon(Icons.power_settings_new_outlined),
+                        iconSize: 20.0,
+                        onPressed: () => changeText()),
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    "Fan",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "$_status",
+                    style: TextStyle(fontSize: 7),
+                  ),
+
+                  //Text()
+                )
+              ]),
+        ),
+      ),
+    ])));
   }
 }
